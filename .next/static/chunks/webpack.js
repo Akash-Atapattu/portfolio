3a218c27c67f9b71c39f9faf9a1a63124ff9e937
atapattua <aatapattu@hugsintl.com>
2023-01-06 18:08:30 -0800

    fixed css styles not loading

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "455cd50ee5447c12"; }
+/******/ 		__webpack_require__.h = function() { return "9dedde1f33994491"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
