1d28738cc5bc2ec10d8627d17172f6777567a854
atapattua <aatapattu@hugsintl.com>
2023-01-06 19:02:10 -0800

    creating welcome message

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9dedde1f33994491"; }
+/******/ 		__webpack_require__.h = function() { return "6d7446c12ff65e7c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import {useState} from 'react';
 // import NavBar from '..components/NavBar';
 
+//accepts time to delay a function in ms
+
+// function delay(time: number){
+//   return new Promise(resolve => setTimeout(resolve, time));
+// }
+
 const Home: NextPage = () => {
+  const welcomes = [
+    "Welcome",
+    "欢迎",
+    "bienvenidas",
+    "willkomen"
+  ]
+  const [welcomeMessage, setWelcomeMessage] = useState("Welcome");
+
+  // for(var i = 0; i < welcomes.length; i++){
+  //   setWelcomeMessage(welcomes[i]);
+  //   delay(1000);
+  //   if(i == welcomes.length - 1){
+  //     i = -1;
+  //   }
+  // }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +40,7 @@ const Home: NextPage = () => {
       <main>
         <div className={styles.background}>
           <div className={styles.homepage}>
-            <p className={styles.title}>Hello</p>
+            <p className={styles.title}>{welcomeMessage}</p>
           </div>
         </div>
       </main>
