4164aa45df9aab23828280809121d825ce714997
Akash Atapattu <aatapattu@hugsintl.com>
2023-01-11 23:56:24 -0800

    fixed background extra margin

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7676c43f28c7b129"; }
+/******/ 		__webpack_require__.h = function() { return "4d0a2c123a7c009c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
